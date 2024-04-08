304c2b6cc35d986a345a43cfb311bee9cff31825
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-04-08 11:29:53 +0200

    project uploda

---
+++ GymTAW/src/main/java/es/uma/gymtaw/GymTawApplication.java
@@ -0,0 +1,13 @@
+package es.uma.gymtaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymTawApplication.class, args);
+    }
+
+}
---
+++ GymTAW/src/test/java/es/uma/gymtaw/GymTawApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.gymtaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
