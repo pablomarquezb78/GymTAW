94deb0c660a2b9f07367a4d6dddc77cc1aa037ff
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-05-05 14:27:41 +0200

    cambio implementacionEjercicioEntity

--- GymTAW/src/main/java/es/uma/dao/ImplementacionEjercicioRutinaRepository.java
+++ GymTAW/src/main/java/es/uma/dao/ImplementacionEjercicioRutinaRepository.java
@@ -1,12 +1,10 @@
 package es.uma.dao;
 
-import es.uma.entity.ImplementacionEjercicioRutina;
 import es.uma.entity.Rutina;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ImplementacionEjercicioRutinaRepository extends JpaRepository<ImplementacionEjercicioRutina, Integer> {
